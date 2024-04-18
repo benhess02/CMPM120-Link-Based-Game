76ee4ba94b5637dbdfc5d0b6f14fa902e100c46a
Ben Hess <pilotben@comcast.net>
2024-04-17 18:07:11 -0700

    Finished project.

--- rules.js
+++ rules.js
@@ -1,43 +1,108 @@
+var hasMainDoorKey = false;
+var isExitUnlocked = false;
+
+var flags = {};
+
 class Start extends Scene {
     create() {
-        this.engine.setTitle("Title goes here"); // TODO: replace this text using this.engine.storyData to find the story title
+        this.engine.setTitle(this.engine.storyData.Title); // TODO: replace this text using this.engine.storyData to find the story title
         this.engine.addChoice("Begin the story");
     }
 
     handleChoice() {
-        this.engine.gotoScene(Location, "Home"); // TODO: replace this text by the initial location of the story
+        this.engine.gotoScene(Location, this.engine.storyData.InitialLocation); // TODO: replace this text by the initial location of the story
     }
 }
 
 class Location extends Scene {
     create(key) {
-        let locationData = undefined; // TODO: use `key` to get the data object for the current story location
-        this.engine.show("Body text goes here"); // TODO: replace this text by the Body of the location data
-        
-        if(true) { // TODO: check if the location has any Choices
-            for(let choice of ["example data"]) { // TODO: loop over the location's Choices
-                this.engine.addChoice("action text"); // TODO: use the Text of the choice
-                // TODO: add a useful second argument to addChoice so that the current code of handleChoice below works
+        let locationData = this.engine.storyData.Locations[key];
+
+        if(locationData.States) {
+            var keys = Object.keys(locationData.States);
+            var found = false;
+            for(var i = 0; i < keys.length; i++) {
+                if(keys[i] != "_" && flags[keys[i]]) {
+                    locationData = locationData.States[keys[i]];
+                    found = true;
+                    break;
+                }
             }
-        } else {
-            this.engine.addChoice("The end.")
+            if(!found) {
+                locationData = locationData.States["_"];
+            }
+        }
+
+        if(locationData.Set) {
+            flags[locationData.Set] = true;
+        }
+
+        this.engine.show(locationData.Body);
+
+        if(locationData.Choices) {
+            for(let choice of locationData.Choices) {
+                this.engine.addChoice(choice.Text, choice);
+            }
         }
     }
 
     handleChoice(choice) {
-        if(choice) {
-            this.engine.show("&gt; "+choice.Text);
-            this.engine.gotoScene(Location, choice.Target);
+        if(choice.Set) {
+            flags[choice.Set] = true;
+        }
+        this.engine.show("&gt; "+choice.Text);
+        this.engine.gotoScene(Location, choice.Target);
+    }
+}
+
+class MainRoomDoor extends Scene {
+    create() {
+        if(hasMainDoorKey) {
+            this.engine.show("You open the door, it leads to a long hallway.");
+            this.engine.addChoice("Enter the hallway", true);
         } else {
-            this.engine.gotoScene(End);
+            this.engine.show("The door is locked.");
+        }
+        this.engine.addChoice("Look around the room", false);
+    }
+
+    handleChoice(enterHallway) {
+        if(enterHallway) {
+            this.engine.show("&gt; Enter the hallway");
+            this.engine.gotoScene(Location, "Hallway");
+        } else {
+            this.engine.show("&gt; Look around the room");
+            this.engine.gotoScene(Location, "MainRoom")
         }
     }
 }
 
-class End extends Scene {
+class CheckTrash extends Scene {
+    create() {
+        if(hasMainDoorKey) {
+            this.engine.show("You look in the trash can, it's empty.");
+        } else {
+            this.engine.show("You look in the trash can, there's a key inside, you pick it up.");
+            hasMainDoorKey = true;
+        }
+        this.engine.addChoice("Look around the room");
+    }
+
+    handleChoice() {
+        this.engine.show("&gt; Look around the room");
+        this.engine.gotoScene(Location, "MainRoom")
+    }
+}
+
+class ControlRoomButton extends Scene {
     create() {
         this.engine.show("<hr>");
-        this.engine.show(this.engine.storyData.Credits);
+    }
+}
+
+class ExitDoor extends Scene {
+    create() {
+        this.engine.show("<hr>");
     }
 }
 
